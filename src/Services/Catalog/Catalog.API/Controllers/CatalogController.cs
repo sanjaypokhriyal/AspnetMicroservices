d8dd9c32da018cae371692b5aa4e90c6c9efabee
pokhriyal.sanjay672@gmail.com <pokhriyal.sanjay672@gmail.com>
2021-07-27 17:56:41 +0530

    Changes for Catalog.API microservice along with docker-compose setup

---
+++ src/Services/Catalog/Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,96 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _repo;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository repo, ILogger<CatalogController> logger)
+        {
+            _repo = repo;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return Ok(await _repo.GetProducts());
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetProduct")]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> GetProductById(string id)
+        {
+            var product = await _repo.GetProduct(id);
+            if (product == null)
+            {
+                _logger.LogError($"Product with id:{id}, not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("[action]/{categoryName}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByCategory(string categoryName)
+        {
+            var product = await _repo.GetProductByCategory(categoryName);
+            if (product == null)
+            {
+                _logger.LogError($"Product with category:{categoryName}, not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpGet("[action]/{name}")]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProductByName(string name)
+        {
+            var product = await _repo.GetProductByName(name);
+            if (product == null)
+            {
+                _logger.LogError($"Product with name:{name}, not found");
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> CreateProduct(Product product)
+        {
+            await _repo.CreateProduct(product);
+            return CreatedAtRoute("GetProduct",new { id = product.Id}, product);
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(Product), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<Product>> UpdateProduct(Product product)
+        {
+            await _repo.UpdateProduct(product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+
+        [HttpDelete("{id:length(24)}",Name = "DeleteProduct")]
+        [ProducesResponseType(typeof(bool), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            return Ok(await _repo.DeleteProduct(id));
+        }
+    }
+}
